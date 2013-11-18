7fcbd325d6543971b5f70bdfbbedc48e062d345c
Damien Pobel <dp@ez.no>
2013-11-18 10:28:24 +0100

    Added a cheatsheet in the configuration file and the empty controller

---
+++ Controller/ForumPhpController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * File containing the ForumPhpController class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace EzSystems\ForumPhp2013DemoBundle\Controller;
+
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+/*
+use eZ\Publish\API\Repository\Values\Content\Location;
+use Symfony\Component\HttpFoundation\Response;
+use eZ\Publish\API\Repository\Values\Content\Query;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
+*/
+
+class ForumPhpController extends Controller
+{
+
+}
