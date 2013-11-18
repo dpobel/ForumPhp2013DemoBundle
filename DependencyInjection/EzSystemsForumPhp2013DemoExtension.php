7eb75606f0760be1eeda0603fea990335202c011
Damien Pobel <dp@ez.no>
2013-11-18 07:31:33 +0100

    Changed the extension class to automatically load the configuration in the bundle

--- DependencyInjection/EzSystemsForumPhp2013DemoExtension.php
+++ DependencyInjection/EzSystemsForumPhp2013DemoExtension.php
@@ -5,14 +5,16 @@ namespace EzSystems\ForumPhp2013DemoBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class EzSystemsForumPhp2013DemoExtension extends Extension
+class EzSystemsForumPhp2013DemoExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +26,11 @@ class EzSystemsForumPhp2013DemoExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend( ContainerBuilder $container )
+    {
+        $config = Yaml::parse( __DIR__ . '/../Resources/config/forumphp.yml' );
+        $container->prependExtensionConfig( 'ezpublish', $config );
     }
 }
